4abb0b22922765293206c1148bac482e80da3c74
or-abdillh <hans.abdillh05@gmail.com>
2023-04-15 09:42:21 +0800

    Create button for scroll to top page

---
+++ resources/js/Components/base/GoToTopButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button ref="button" @click="clicked"
+        class="fixed right-5 bottom-28 rounded-full bg-gray-100 bg-opacity-50 backdrop-blur w-12 h-12 border-2 border-gray-300 text-gray-800 opacity-0 duration-300"
+        title="Go To Top"><i class="fa-solid fa-arrow-up-long"></i></button>
+</template>
+
+<script setup>
+
+import { onMounted, ref } from 'vue'
+
+const button = ref(null)
+
+const clicked = () => {
+    document.querySelector('#app').scrollIntoView({
+        behavior: 'smooth',
+    })
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 300) {
+            button.value.classList.replace('opacity-0', 'opacity-1')
+        } else {
+            button.value.classList.replace('opacity-1', 'opacity-0')
+        }
+    })
+})
+
+</script>
--- resources/js/Layouts/BaseLayout.vue
+++ resources/js/Layouts/BaseLayout.vue
@@ -3,6 +3,7 @@
     <main>
         <slot />
     </main>
+    <GoToTopButton></GoToTopButton>
     <Footer></Footer>
 </template>
 
@@ -10,5 +11,6 @@
 
 import NavigationBar from '@/Components/base/NavigationBar.vue'
 import Footer from '@/Components/base/Footer.vue'
+import GoToTopButton from '@/Components/base/GoToTopButton.vue'
 
 </script>
